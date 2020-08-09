1331968fd4a84ab20a75ac1ece436168bf0ad251
Ashwin <achchu.rai@gmail.com>
2020-08-09 14:58:31 +0530

    add log Main Branch

--- app/src/main/java/com/mystartup/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/mystartup/animationsinandroid/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MyMessage","Android");
+        Log.i("SpaceX","Falcon 9");
 
     }
 }
